3c26abf52870fc894eb9607c127f89027a989fcd
{Dwanx-9} <56589888+Dwanx-9@users.noreply.github.com>
2023-12-21 03:22:59 +0700

    Update ProfileSection.jsx

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -2,7 +2,7 @@ import Image from "next/image"
 const ProfileSection = () => {
     return (
         <div class="group flex items-center">
-        <img class="shrink-0 h-12 w-12 rounded-full" src="https://i.gifer.com/fxVE.gif" alt="Profile" />
+        <img class="shrink-0 h-12 w-12 rounded-full" src="https://i.gifer.com/SVKl.gif" alt="Profile" />
         <div class="ltr:ml-3 rtl:mr-9">
           <p class="text-sm font-medium text-slate-300 group-hover:text-white">Dimassell</p>
           <p class="text-sm font-medium text-slate-500 group-hover:text-slate-300">Social Media Specialist</p>
