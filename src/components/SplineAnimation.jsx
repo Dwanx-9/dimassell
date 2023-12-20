e8d33801a96ea9794cfa8641eb322f9397722424
{Dwanx-9} <56589888+Dwanx-9@users.noreply.github.com>
2023-12-21 05:43:44 +0700

    Update SplineAnimation.jsx

--- src/components/SplineAnimation.jsx
+++ src/components/SplineAnimation.jsx
@@ -5,7 +5,7 @@ const SplineAnimation = () => {
   return (
 
     // <Spline scene="https://prod.spline.design/u9vxsHGKnJqRgSov/scene.splinecode" />
-     <Spline scene='https://prod.spline.design/OWV1dCWPosNXKAQb/scene.splinecode' />
+     <Spline scene="https://prod.spline.design/lcgExCS7JMXM2SZg/scene.splinecode" />
     // <Spline scene="https://prod.spline.design/zRHXTS2YCyVpLuXe/scene.splinecode" />
   
 
