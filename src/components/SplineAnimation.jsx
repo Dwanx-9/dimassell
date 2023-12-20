29b3d2c9b9716b8524f7948b133dd36bfd965fb6
{Dwanx-9} <56589888+Dwanx-9@users.noreply.github.com>
2023-12-21 04:54:07 +0700

    Update SplineAnimation.jsx

--- src/components/SplineAnimation.jsx
+++ src/components/SplineAnimation.jsx
@@ -4,8 +4,8 @@ import Spline from '@splinetool/react-spline';
 const SplineAnimation = () => {
   return (
 
-    <Spline scene="https://prod.spline.design/u9vxsHGKnJqRgSov/scene.splinecode" />
-
+    // <Spline scene="https://prod.spline.design/u9vxsHGKnJqRgSov/scene.splinecode" />
+     <Spline scene='https://prod.spline.design/OWV1dCWPosNXKAQb/scene.splinecode' />
     // <Spline scene="https://prod.spline.design/zRHXTS2YCyVpLuXe/scene.splinecode" />
   
 
