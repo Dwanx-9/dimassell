f06101add55d6678014ae91f6cca153e4e79230a
{Dwanx-9} <56589888+Dwanx-9@users.noreply.github.com>
2023-12-21 03:18:21 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     const [navbarOpen, setNavbarOpen] = useState(false);
  
     return (
-        <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 bg-orange-600 opacity-70 '>
+        <nav className='fixed mx-auto border border-[#33353F] top-0 left-0 right-0 z-10 backdrop-blur-sm  transition-all ease-out-expo duration-700  bg-transparent '>
              <div className='flex container lg:py-4 flex-wrap items-center justify-between mx-auto px-4 py-2'>
                 <Link href={"/"} className="text-2xl md:text-5xl text-white font-semibold">
                  DX1
