b4f9dc1b0c46ec06ae13c11f9f53cfd2da8e20c9
{Dwanx-9} <56589888+Dwanx-9@users.noreply.github.com>
2023-12-21 03:46:26 +0700

    Update MessageSelection.jsx

--- src/components/MessageSelection.jsx
+++ src/components/MessageSelection.jsx
@@ -12,8 +12,10 @@ const MessageSection = () => {
         Connect With Me
       </p>
     </div>
+      <a href="https://t.me/Orangbawah9" target="_blank">
     <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
-  </div>
+        </a>
+      </div>
 </div>
     )
 }
