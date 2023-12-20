4a2c65fd6afa00fb664c241acf3ae8049b0a4f04
{Dwanx-9} <56589888+Dwanx-9@users.noreply.github.com>
2023-12-21 03:32:06 +0700

    Update MessageSelection.jsx

--- src/components/MessageSelection.jsx
+++ src/components/MessageSelection.jsx
@@ -2,14 +2,14 @@
 const MessageSection = () => {
     return (
         <div class="py-8 px-8 max-w-sm mx-auto bg-primary-500 rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6 ">
-  <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://i.gifer.com/Qaf1.gif" alt="Dimassell" />
+  <img class="block mx-auto h-24 rounded-full sm:mx-0 sm:shrink-0" src="https://i.gifer.com/SVKl.gif" alt="Dimassell" />
   <div class="text-center space-y-2 sm:text-left">
     <div class="space-y-0.5">
       <p class="text-lg text-black font-semibold">
         Dimassell
       </p>
       <p class="text-slate-700 font-medium">
-        Full Stuck Developer
+        Connect With Me
       </p>
     </div>
     <button class="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
